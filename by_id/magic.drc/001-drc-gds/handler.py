b7e4303dadee402d379e0a3a5f943275a39136dd
kareefardi <kareem.farid@efabless.com>
2023-10-26 13:07:30 +0300

    lint

--- by_id/magic.drc/001-drc-gds/handler.py
+++ by_id/magic.drc/001-drc-gds/handler.py
@@ -5,5 +5,5 @@ def handle(step):
     drc_count = step.state_out.metrics[MagicDRC.metric_name]
     assert drc_count > 0, f"Expected DRC: > 0, Found: {drc_count}"
     assert (
-        step.config["MAGIC_DRC_USE_GDS"] == True
+        step.config["MAGIC_DRC_USE_GDS"] is True
     ), "MAGIC_DRC_USE_GDS is disabled. This test runs on GDS."
