06d2ffd4b7a2026110f90630e51bb1bf00aefa98
Mohamed Gaber <donn@efabless.com>
2023-10-30 20:12:50 +0200

    Mass de-duplication

--- by_id/magic.streamout/004-success-hybrid/handler.py
+++ by_id/magic.streamout/004-success-hybrid/handler.py
@@ -1,10 +1,1 @@
-import subprocess
-
-
-def handle(step):
-    result = subprocess.call(
-        ["klayout", "-zz", step.state_out["mag_gds"]],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
-    assert result == 0, "Couldn't open generated GDS."
+../001-success/handler.py
