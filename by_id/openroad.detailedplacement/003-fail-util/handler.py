14f567881af752f97aca5b0aa7b73b16c2a0c9c4
kareefardi <kareem.farid@efabless.com>
2023-11-15 18:16:00 +0200

    remove a bunch of odb files and do a couple of symlinks

--- by_id/openroad.detailedplacement/003-fail-util/handler.py
+++ by_id/openroad.detailedplacement/003-fail-util/handler.py
@@ -1,7 +1,1 @@
-from openlane.steps import StepError
-
-
-def handle(exception: StepError, caplog):
-    error_msg = "[ERROR DPL-0036] Detailed placement failed."
-    assert exception is not None, "Util > 100% did not raise detailed placement error."
-    assert error_msg in caplog.text, f"{error_msg} not found in log file"
+../002-fail-padding/handler.py
