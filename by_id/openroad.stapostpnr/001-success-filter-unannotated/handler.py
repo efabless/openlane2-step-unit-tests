2a54a15b161e55c075e16c96726976c09730e635
kareefardi <kareem.farid@efabless.com>
2023-11-19 18:13:56 +0200

    formatting

--- by_id/openroad.stapostpnr/001-success-filter-unannotated/handler.py
+++ by_id/openroad.stapostpnr/001-success-filter-unannotated/handler.py
@@ -4,4 +4,4 @@ def handle(step):
     ), f"Error: Expected timing__unannotated_nets__count 392: Found {step.state_out.metrics['timing__unannotated_nets__count']}"
     assert (
         step.state_out.metrics["timing__unannotated_nets_filtered__count"] == 68
-    ),f"Error: Expected timing__unannotated_nets_filtered__count 68: Found {step.state_out.metrics['timing__unannotated_nets_filtered__count']}"
+    ), f"Error: Expected timing__unannotated_nets_filtered__count 68: Found {step.state_out.metrics['timing__unannotated_nets_filtered__count']}"
