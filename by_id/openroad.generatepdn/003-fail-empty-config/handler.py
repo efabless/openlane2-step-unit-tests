c158d609876c556e4aa3704c1fbd068a3d5216e6
Mohamed Gaber <donn@efabless.com>
2023-12-21 15:17:53 +0200

    Rename metric

--- by_id/openroad.generatepdn/003-fail-empty-config/handler.py
+++ by_id/openroad.generatepdn/003-fail-empty-config/handler.py
@@ -1,8 +1,8 @@
 def handle(step, exception, caplog):
     assert exception is None
     assert (
-        step.state_out.metrics["power_grid__violation__count__net:VPWR"] > 0
+        step.state_out.metrics["design__power_grid_violation__count__net:VPWR"] > 0
     ), "Completely missing PDN did not report any VPWR nodes as disconnected"
     assert (
-        step.state_out.metrics["power_grid__violation__count__net:VGND"] > 0
+        step.state_out.metrics["design__power_grid_violation__count__net:VGND"] > 0
     ), "Completely missing PDN did not report any VGND nodes disconnected"
--- by_id/openroad.generatepdn/007-fail-misconnect/handler.py
+++ by_id/openroad.generatepdn/007-fail-misconnect/handler.py
@@ -1,8 +1,8 @@
 def handle(step, exception, caplog):
     assert exception is None
     assert (
-        step.state_out.metrics["power_grid__violation__count__net:VPWR"] > 0
+        step.state_out.metrics["design__power_grid_violation__count__net:VPWR"] > 0
     ), "Misconfigured PDN has no VPWR violations"
     assert (
-        step.state_out.metrics["power_grid__violation__count__net:VGND"] > 0
+        step.state_out.metrics["design__power_grid_violation__count__net:VGND"] > 0
     ), "Misconfigured PDN has no VGND violations"
