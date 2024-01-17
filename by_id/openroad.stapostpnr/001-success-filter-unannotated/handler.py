cedaedd092cdc07a5f2d74a36b4e499d0df26c41
Kareem Farid <kareefardi@users.noreply.github.com>
2024-01-17 19:43:38 +0200

    fix failing test

--- by_id/openroad.stapostpnr/001-success-filter-unannotated/handler.py
+++ by_id/openroad.stapostpnr/001-success-filter-unannotated/handler.py
@@ -3,5 +3,5 @@ def handle(step):
         step.state_out.metrics["timing__unannotated_net__count"] == 392
     ), f"Error: Expected timing__unannotated_net__count 392: Found {step.state_out.metrics['timing__unannotated_net__count']}"
     assert (
-        step.state_out.metrics["timing__unannotated_net__filtered_count"] == 68
-    ), f"Error: Expected timing__unannotated_net__filtered_count 68: Found {step.state_out.metrics['timing__unannotated_net__filtered_count']}"
+        step.state_out.metrics["timing__unannotated_net_filtered__count"] == 68
+    ), f"Error: Expected timing__unannotated_net_filtered__count 68: Found {step.state_out.metrics['timing__unannotated_net_filtered__count']}"
