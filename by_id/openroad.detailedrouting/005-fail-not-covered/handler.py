62805b7e43538c1c2eb6e0081ae9bb404c1e602d
Mohamed Gaber <donn@efabless.com>
2024-04-24 12:00:51 +0200

    Add previous GRT failure as a test

--- by_id/openroad.detailedrouting/005-fail-not-covered/handler.py
+++ by_id/openroad.detailedrouting/005-fail-not-covered/handler.py
@@ -1,7 +1,8 @@
 def handle(step, exception, caplog, openroad_alerts):
     assert exception is not None, "Detalied router didn't raise an exception"
-    # [ERROR DRT-0218] Guide is not connected to design
-    error = "DRT-0218"
+    print(openroad_alerts)
+    # [ERROR DRT-0155] Guide is not connected to design
+    error = "DRT-0155"
     assert error in [
         alert.code for alert in openroad_alerts
     ], f"No alerts for {error} found"
